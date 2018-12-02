87dfd2dbd307d00713651506b4e9eabd632126f5
AbdullahAlbatairy <45461751+AbdullahAlbatairy@users.noreply.github.com>
2018-12-02 14:41:35 +0300

    Add files via upload

---
+++ Project_SWE205/src/StudentUI.java
@@ -0,0 +1,88 @@
+package StudentSystem;
+
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class StudentUI extends Application implements EventHandler {
+    private Stage window;
+    private Button logOut = new Button("Log Out");
+    private Button BackButton = new Button("Back");
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("Home");
+        try {
+            window.setScene(homeScene());
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        window.show();
+
+    }
+
+
+    private Scene homeScene() throws FileNotFoundException {
+        Scanner readFile = new Scanner(new FileInputStream("Menu Buttons.txt"));
+        BorderPane pane = new BorderPane();
+        VBox box = new VBox();
+        Button[] sideButton = new Button[4];
+
+        logOut.setPrefWidth(80);
+        for (int i = 0; i < sideButton.length; i++) {
+            sideButton[i] = new Button();
+            sideButton[i].setText(readFile.nextLine());
+            sideButton[i].setOnAction(this);
+            sideButton[i].setPrefWidth(120);
+            mouseAffect(sideButton[i]);
+            box.getChildren().add(sideButton[i]);
+        }
+
+
+        box.setSpacing(15);
+        box.setPrefWidth(95);
+        box.setAlignment(Pos.CENTER);
+        Label label = new Label("Heelo");
+        label.setPrefWidth(90);
+        logOut.setPrefWidth(90);
+        pane.setCenter(box);
+        pane.setRight(logOut);
+        pane.setLeft(label);
+        return new Scene(pane, 400, 400);
+    }
+
+    private void mouseAffect(Button b) {
+        b.setStyle("-fx-background-color: #008000;-fx-text-fill:white");
+        b.setOnMouseEntered(e -> b.setStyle("-fx-background-color: #19662d;-fx-text-fill:white"));
+        b.setOnMouseExited(e -> b.setStyle("-fx-background-color: #008000;-fx-text-fill:white"));
+    }
+
+    @Override
+    public void handle(Event event) {
+        Button b = (Button) event.getTarget();
+        if (b.equals("Registration"))
+            window.setScene(RegistrationScene());
+    }
+
+    private Scene RegistrationScene() {
+        BorderPane outerLay = new BorderPane();
+
+        return new Scene(outerLay,400,400);
+    }
+}
+
